12ffa34da445bb1032a2a4632085267bb001941b
Sunnefa Lind <sunnefa_lind@hotmail.com>
2013-09-13 13:45:57 +0000

    Added pagination of posts
    It's a bit dirty what with not using a template for the pagination
    output but it works

--- main.php
+++ main.php
@@ -8,16 +8,48 @@
 switch($show) {
 	case 'posts':
 		//get the data from the database
-		$posts = $database->get_data("SELECT title, content, FROM_UNIXTIME(timestamp, '%D %M %Y') as date FROM posts ORDER BY timestamp DESC", false, true);
+		$all_posts = $database->get_data("SELECT title, content, FROM_UNIXTIME(timestamp, '%D %M %Y') as date FROM posts ORDER BY timestamp DESC", false, true);
 		
+                $all_posts_count = count($all_posts);
+                
+                $last_page = ceil($all_posts_count / 5);
+                
+                $posts = array_chunk($all_posts, 5);
+                
+                $page_count = count($posts);
+                
+                $p = (isset($queries[1])) ? $queries[1]-1 : 0;
+                
+                if($p > $last_page) $p = $last_page;
+                
 		//process the HTML template
 		echo '<h1>Latest news</h1>';
-		foreach($posts as $post) {
+		foreach($posts[$p] as $post) {
 			$theme->html_small($post_template);
 			$theme->replace_tokens(array('POST_TITLE' => $post['title'], 'POST_CONTENT' => $post['content'], 'POST_DATE' => $post['date']));
 			echo $theme->display();
 		}
+                echo '<p>';
+                if($p > 0) {
+                    echo '<a href="/posts/1"><<</a> [...]';
+                }
+                $range = 3;
 		
+                for($i = (($p + 1) - $range); $i < (($p + $range) + 2); $i++) {
+                    if($i >= 1 && $i <= $page_count) {
+                        if($i == $p+1) {
+                            echo ' <b>' . $i . '</b> ';
+                        } else {
+                            echo ' <a href="/posts/' . $i . '">' . $i . '</a> ';
+                        }
+                    }
+                }
+                
+                if(($p + 1) != $last_page) {
+                    echo '[...] <a href="/posts/' . $last_page . '">>></a>';
+                }
+                
+                echo '</p>';
 		break;
 	
 	case 'cat':
